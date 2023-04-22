0ddc53b150ace2f37b221d71866af5e361c7ff9f
Kirill Khomicevich <khomicevichkirill@icloud.com>
2023-04-22 19:45:37 +0300

    Initial Commit

---
+++ simple_VIPER/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  simple_VIPER
+//
+//  Created by Kirill Khomytsevych on 22.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
